7e2b2df367dee23fa5085f090a43b489a4f14d35
swindlesmccoop <swindlesmccoop@waifu.club>
2022-08-18 15:09:45 -0400

    Preliminary script

--- dps.sh
+++ dps.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+command -v sudo > /dev/null && ROOTCOMMAND=sudo || ROOTCOMMAND=doas
+cd src
+[ -f patches.full ] || grep ".*PATCH [0-1]" > patches.list
+
+grep ".*PATCH [0-1]" patches.list | sed 's/#define //g' | sed 's/[0-1]//g' | fzf --multi | sed 's/$/1/g' | sed 's/^/#define /g' > patches.def.h
+
+make
