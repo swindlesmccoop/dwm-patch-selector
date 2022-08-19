bc402fce7e4be0401851672d28d8a42e356b5436
swindlesmccoop <swindlesmccoop@waifu.club>
2022-08-19 04:44:22 +0000

    Add more stuff

--- dps.sh
+++ dps.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
 command -v sudo > /dev/null && ROOTCOMMAND=sudo || ROOTCOMMAND=doas
-cd src
-[ -f patches.full ] || grep ".*PATCH [0-1]" > patches.list
+[ "$ROOTCOMMAND" = "sudo" ] && MAKECOMMAND=make|| MAKECOMMAND=gmake
 
-grep ".*PATCH [0-1]" patches.list | sed 's/#define //g' | sed 's/[0-1]//g' | fzf --multi | sed 's/$/1/g' | sed 's/^/#define /g' > patches.def.h
+finalize() {
+	cd ..
+	cp -r dwm-flexipatch dwm-flexipatch-bak
+	cd flexipatch-finalizer
+	./flexipatch-finalizer.sh --run --directory ../dwm-flexipatch --output ../final-src
+	cd ..
+	rm -rf dwm-flexipatch
+	mv dwm-flexipatch-bak dwm-flexipatch
+	cd final-src
+	$MAKECOMMAND && $ROOTCOMMAND $MAKECOMMAND install
+	printf "\-\-\-\-\nDone\n\-\-\-\-\n"
+}
 
-make
+cd dwm-flexipatch
+[ -f patches.list ] || grep ".*PATCH [0-1]" patches.def.h > patches.list
+sed -i 's/_PATCH 1/_PATCH 0/g' patches.list
+
+grep "_PATCH [0-1]" patches.list | sed 's/#define //g' | sed 's/_PATCH 0//g' | tr '[:upper:]' '[:lower:]' | fzf --multi | tr '[:lower:]' '[:upper:]' | sed 's/$/_PATCH 1/g' | sed 's/^/#define /g' > patches.def.h
+grep -v "$(cat patches.def.h)" patches.list >> patches.def.h
+sed -i 's/BAR_LTSYMBOL_PATCH 0/BAR_LTSYMBOL_PATCH 1/' patches.def.h
+sed -i 's/BAR_STATUS_PATCH 0/BAR_STATUS_PATCH 1/' patches.def.h
+sed -i 's/BAR_TAGS_PATCH 0/BAR_TAGS_PATCH 1/' patches.def.h
+sed -i 's/BAR_WINTITLE_PATCH 0/BAR_WINTITLE_PATCH 1/' patches.def.h
+
+printf "Finalize build and strip flexipatch components? (y/n): "
+read FINALIZE
+
+case "$FINALIZE" in
+	y) finalize && exit ;;
+	n) $MAKECOMMAND && $ROOTCOMMAND $MAKECOMMAND install && printf "\-\-\-\-\nDone\n\-\-\-\-\n" ;;
+	*) printf "Invalid input, exiting...\n" ;;
+esac
